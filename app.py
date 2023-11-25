0d23ede27194ac4bfb045f1a44a6561e2d959799
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-11-24 20:41:59 -0800

    refactored with session cookie

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import *
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -7,23 +7,27 @@ app.config["SECRET_KEY"] = "shhhh!!"
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
 current_survey = satisfaction_survey
 
 @app.route("/")
 def index_route():
     return render_template("instructions.html", survey = current_survey)
 
+@app.route("/new", methods=["POST"])
+def start_new_session():
+    session["responses"] = []
+    return redirect("/questions/0")
+
 @app.route("/questions/<question_num>")
 def question_route(question_num):
-    if int(question_num) == len(responses):
+    if int(question_num) == len(session["responses"]):
         return render_template("question.html",
                            question_num = int(question_num), survey = current_survey,
                            question=current_survey.questions[int(question_num)])
     else:
-        if len(responses) < len(current_survey.questions):
+        if len(session["responses"]) < len(current_survey.questions):
             flash("Questions must be done in order!")
-            return redirect("/questions/" + str(len(responses)))
+            return redirect("/questions/" + str(len(session["responses"])))
         else:
             flash("The survey has already been completed")
             return redirect("/thank-you")
@@ -32,15 +36,17 @@ def question_route(question_num):
 def record_answer():
     ans = request.form.get("survey-question", None)
     
-    if len(responses) > int(request.referrer[-1]):
+    if len(session["responses"]) > int(request.referrer[-1]):
         flash ("Question already answered!")
     elif ans:
-        responses.append(ans)
+        rsps = session["responses"]
+        rsps.append(ans)
+        session["responses"] = rsps
     else:
         flash("Answer Required!")
     
-    if len(responses) < len(current_survey.questions):
-        return redirect("/questions/" + str(len(responses)))
+    if len(session["responses"]) < len(current_survey.questions):
+        return redirect("/questions/" + str(len(session["responses"])))
         # this should go to the next page if you answered or back to the current one
         # if you did not
     else:
@@ -48,4 +54,4 @@ def record_answer():
     
 @app.route("/thank-you")
 def thank_you():
-    return render_template("thank-you.html", survey=current_survey, responses=responses)
+    return render_template("thank-you.html", survey=current_survey, responses=session["responses"])
