a1502d374fa39876d32b9f6d3e20d40bb75f2092
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-11-24 19:16:23 -0800

    step 2 complete

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, render_template
 from surveys import *
+from flask_debugtoolbar import DebugToolbarExtension
 
 app=Flask(__name__)
+app.config["SECRET_KEY"] = "shhhh!!"
+debug = DebugToolbarExtension(app)
 
 responses = []
 
+@app.route("/")
+def index_route():
+    return render_template("satisfaction.html")
